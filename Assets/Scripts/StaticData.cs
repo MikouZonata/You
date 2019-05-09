05595cb01b8b9362b06c14944085b3cb22aa7768
MikouZonata <kevin.vanas@student.hku.nl>
2019-05-09 12:21:39 +0200

    maan updates

--- Assets/Scripts/StaticData.cs
+++ Assets/Scripts/StaticData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public static class StaticData
 {
 	public enum PlayerOptions { Kevin, Maan };
-	public static PlayerOptions[] playerOptions = new PlayerOptions[] { PlayerOptions.Maan, PlayerOptions.Maan };
+	public static PlayerOptions[] playerOptions = new PlayerOptions[] { PlayerOptions.Maan, PlayerOptions.Kevin };
 	public static int levelNumber = 1;
 
 	public static Transform[] playerTransforms = new Transform[2];
