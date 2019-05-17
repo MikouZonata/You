168d0aad7abfa3ebf81312c852c8d14ce11da9f1
MikouZonata <kevin.vanas@student.hku.nl>
2019-05-17 17:14:25 +0200

    beetje kattoe

--- Assets/Scripts/Kattoe.cs
+++ Assets/Scripts/Kattoe.cs
@@ -11,11 +11,14 @@ public class Kattoe : MonoBehaviour
 
 	bool attachedToMaan = false;
 
-	float minTimeBetweenRoaming = 1, maxTimeBetweenRoaming = 3.6f;
-
-	float callFrequencyNotAttached = 2.5f, callFrequencyAttached = 1.2f, callFrequencyDeviation = .2f;
+	float callFrequencyNotAttached = 2.5f, callFrequencyAttached = 1.2f, callFrequencyDeviation = .3f;
 	float _callTimer; 
 	float callPitchMax = 1.5f, callPitchMin = 0.2f;
+
+	float minTimeBetweenRoaming = 1, maxTimeBetweenRoaming = 3.6f;
+	Vector3 basePosition;
+	float maxRoamingDistance = 7;
+
 	int chanceToTempt = 40;
 
 	public void Init (AudioClip callClip)
@@ -25,8 +28,10 @@ public class Kattoe : MonoBehaviour
 
 		this.callClip = callClip;
 		audioSource.clip = callClip;
+		navMeshAgent = GetComponent<NavMeshAgent>();
 
 		_callTimer = Random.Range(-callFrequencyDeviation, callFrequencyDeviation);
+		basePosition = transform.position;
 
 		StartCoroutine(Roam());
 	}
@@ -54,19 +59,43 @@ public class Kattoe : MonoBehaviour
 
 	IEnumerator Roam ()
 	{
+		float waitTime = Random.Range(minTimeBetweenRoaming, maxTimeBetweenRoaming);
+		float _timer = 0;
 		while (true) {
 			if (attachedToMaan) {
 				StartCoroutine(FollowMaan());
 				yield break;
 			}
-
+			_timer += Time.deltaTime;
+			if (_timer >= waitTime) {
+				break;
+			}
 			yield return null;
+		}
+
+		Vector2 randomPoint = Random.insideUnitCircle * maxRoamingDistance;
+		Vector3 targetPosition = basePosition + new Vector3(randomPoint.x, 0, randomPoint.y);
+		navMeshAgent.destination = targetPosition;
+
+		while ((transform.position - targetPosition).sqrMagnitude > .5f) {
+			if (attachedToMaan) {
+				StartCoroutine(FollowMaan());
+				yield break;
+			}
+			yield return null;
+		}
+
+		if (attachedToMaan) {
+			StartCoroutine(FollowMaan());
+		} else {
+			StartCoroutine(Roam());
 		}
 	}
 
 	IEnumerator FollowMaan ()
 	{
-
+		navMeshAgent.enabled = false;
+		yield return null;
 	}
 
 	public bool Tempt ()
